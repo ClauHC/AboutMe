9fc248136276ff3d2464fd2c7021a005dd89f5d4
ClauHC <clauhc90@gmail.com>
2024-12-10 18:49:22 +0100

    Initial commit

---
+++ src/HCCMain.java
@@ -0,0 +1,17 @@
+//Ejercicio 1 About Me
+//Claudia Hernandez Colomer
+//Fp DAW La Salle
+//Profesora Marta Bella
+//diciembre 2024
+
+//Empiezo así para poder usar clases y métodos
+public class HCCMain {
+    public static void main(String[] args) {
+        HCCMain programa = new HCCMain();
+        programa.inicio();
+    }
+
+    public void inicio(){
+        //Aqui va lo de Main (HCCMain (Hernandez Colomer Claudia Main) en este caso luego del refactor)
+    }
+}
