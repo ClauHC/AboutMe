391571c2895a3ceb995a2816400513d77e2dbacd
ClauHC <clauhc90@gmail.com>
2024-12-11 16:01:28 +0100

    Creation of the class getter methods

--- src/Perfil.java
+++ src/Perfil.java
@@ -6,9 +6,7 @@ Define la clase Perfil con las siguientes características:
 • hobbies: Colección de hobbies del perfil.
 • foods: Colección de comidas favoritas del perfil.
 • funFacts: Colección de datos curiosos.
-La clase deberá tener métodos para acceder a estos atributos y métodos que devuelvan la información
-de manera formateada para que sea fácil de mostrar por consola la información
- */
+*/
 
 // Atributos (los atributos de una clase deben ser de prefrerencia privados)
 public class Perfil {
@@ -20,6 +18,19 @@ public class Perfil {
     private String[] foods;
     private String[] funFacts;
 
+    /*
+    // Constructor de una persona aburrida.
+    //Esto me lo estoy inventando aparte de la orden del ejercicio, pens&eacute; que puede haber gente que no tenga hobbies, ni comidas favoritas ni nada que contar.
+    public Perfil(String name, String story) {
+        this.name = name;
+        this.story = story;
+        // Valores predeterminados
+        this.hobbies = new String[] {"No tengo hobbies", "La pereza me puede como para buscarme un hobbie"};
+        this.foods = new String[] {"No tengo comida favorita", "Como cualquier cosa"};
+        this.funFacts = new String[] {"No tengo nada que contar"};
+    }
+    */
+
     // Constructor
     public Perfil(String name, String story, String[] hobbies, String[] foods, String[] funFacts) {
         this.name = name;
@@ -27,6 +38,32 @@ public class Perfil {
         this.hobbies = hobbies;
         this.foods = foods;
         this.funFacts = funFacts;
+    }
+
+/*
+La clase deberá tener métodos para acceder a estos atributos y métodos que devuelvan la información
+de manera formateada para que sea fácil de mostrar por consola la información
+ */
+
+    // Geetter method == read method. Cuando un métodoo permite obtener el valor de un atributo privado (apunte de clase)
+    public String getName() {
+        return name;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public String[] getFoods() {
+        return foods;
+    }
+
+    public String[] getFunFacts() {
+        return funFacts;
     }
 }
 
